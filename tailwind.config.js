75be8eac135d3c928cd77bd829e625950a39e226
Hillel1408 <etemax@bk.ru>
2023-04-21 14:39:47 +0300

    character

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,31 @@
-const options = require("./config"); //options from config.js
+const options = require('./config'); //options from config.js
 
 const allPlugins = {
-  typography: require("@tailwindcss/typography"),
-  forms: require("@tailwindcss/forms"),
-  containerQueries: require("@tailwindcss/container-queries"),
+    typography: require('@tailwindcss/typography'),
+    forms: require('@tailwindcss/forms'),
+    containerQueries: require('@tailwindcss/container-queries'),
 };
 
 const plugins = Object.keys(allPlugins)
-  .filter((k) => options.plugins[k])
-  .map((k) => {
-    if (k in options.plugins && options.plugins[k]) {
-      return allPlugins[k];
-    }
-  });
+    .filter((k) => options.plugins[k])
+    .map((k) => {
+        if (k in options.plugins && options.plugins[k]) {
+            return allPlugins[k];
+        }
+    });
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,php}"],
-  darkMode: "class",
-  theme: {
-    extend: {},
-  },
-  plugins: plugins,
+    content: ['./src/**/*.{html,js,php}'],
+    darkMode: 'class',
+    theme: {
+        extend: {
+            colors: {
+                brown: '#6D5E3C',
+                green: '#5C704E',
+                lightBrown: '#EDEAE7',
+            },
+        },
+    },
+    plugins: plugins,
 };
