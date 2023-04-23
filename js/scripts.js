942dd7bc58757d8e3f54e719833bfc01c0e6a2e5
Hillel1408 <etemax@bk.ru>
2023-04-23 13:50:43 +0300

    Updates

---
+++ js/scripts.js
@@ -0,0 +1,42 @@
+//libraries like jquery etc
+window.addEventListener('DOMContentLoaded', () => {
+    const swiper = new Swiper('.swiper-image', {
+        slidesPerView: 1,
+        pagination: {
+            el: '.swiper-pagination',
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {
+            640: {
+                slidesPerView: 3,
+            },
+        },
+        spaceBetween: 20,
+    });
+    const swiperReviews = new Swiper('.swiper-reviews', {
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+    const mobileMenu = document.querySelector('.mobile-menu');
+    const openBtn = document.querySelector('.mobile-menu__open');
+    const closeBtn = document.querySelector('.mobile-menu__close');
+
+    openBtn.addEventListener('click', () => {
+        mobileMenu.classList.toggle('active');
+        document.body.style.overflow = 'hidden';
+    });
+
+    closeBtn.addEventListener('click', () => {
+        mobileMenu.classList.toggle('active');
+        document.body.style.overflow = 'initial';
+    });
+});
